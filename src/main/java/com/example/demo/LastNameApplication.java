a75752432642416c54b3520be8db7f4c0f86d6a5
faleot724 <faleot724@gmail.com>
2023-06-06 15:49:18 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/LastNameApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastNameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LastNameApplication.class, args);
+	}
+
+}
